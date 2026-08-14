62c7fee9e49ce2b8bc2804fbf161974b2d20d6f2
Eigram.Buenaventura <eigram.buenaventura02@gmail.com>
2026-08-14 13:46:37 +0800

    Update IT Inventory Management

--- vite.config.ts
+++ vite.config.ts
@@ -81,7 +81,7 @@ function figmaSiteConfiguration(config: FigmaSiteConfiguration): Plugin {
     return html.replace(`<!-- ${slotName} -->`, content)
   }
 
-  const title = config.title ?? "IT Inventory Test Site"
+  const title = config.title ?? "IT Testing Page"
   const description = config.description ?? ''
   const favicon = config.icons?.icon ?? ''
   const socialImage = config.openGraph?.image ?? ''
